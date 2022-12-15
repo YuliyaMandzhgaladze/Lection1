14d53dd8525530aa85f01482eb8967c833eed054
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-15 23:58:36 +0400

    Решила первую задачу!!!САМА!

--- Sem111dz/Program.cs
+++ Sem111dz/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a == b*b)
+{
+  Console.Write("Первое квадрат второго");
+}
+else
+{ Console.WriteLine("Первое не квадрат второго");
+}
