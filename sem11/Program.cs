316a2472e4edd987af53667dadbc0c1fe6c95b55
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-15 23:20:36 +0400

    Закончили семинар, буду разбираться

---
+++ sem11/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a>b)
+{
+  Console.Write("большее число: ");
+Console.WriteLine(a); 
+Console.Write("меньшее число: ");
+Console.WriteLine(b); 
+}
+else 
+{
+    Console.Write("большее число: ");
+Console.WriteLine(b); 
+Console.Write("меньшее число: ");
+Console.WriteLine(a);  
+}
