90aaefc7a16e89db2405f9b8861d184f562dafcf
Yulya <yuliyamandzhgaladze@yandex.ru>
2022-12-16 11:19:09 +0400

    привела в порядок

--- Sem111dz/Program.cs
+++ Sem111dz/Program.cs
@@ -3,9 +3,6 @@ int a = int.Parse(Console.ReadLine()!);
 Console.Write("введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 if (a == b*b)
-{
-  Console.Write("Первое квадрат второго");
-}
+Console.Write("Первое число квадрат второго");
 else
-{ Console.WriteLine("Первое не квадрат второго");
-}
+Console.WriteLine("Первое число НЕ квадрат второго");
